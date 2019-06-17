4e4f2a58aa0c1376608a0876e040aafba7ab9747
sgtbrunner <guilherme.brunner@gmail.com>
2019-06-17 15:32:56 -0300

    adding build

---
+++ build/precache-manifest.23b42ee782d03ac18c1f2ece46b71dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab1f422aa9538261cb4069b2e38e87d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "./static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "765b18b500aa89e64d47",
+    "url": "./static/css/main.d5f2def9.chunk.css"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "./static/js/2.33497142.chunk.js"
+  },
+  {
+    "revision": "765b18b500aa89e64d47",
+    "url": "./static/js/main.fb31f918.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7467e8cbffec41a891c63249641ecd86",
+    "url": "./static/media/brain.7467e8cb.png"
+  }
+]);
